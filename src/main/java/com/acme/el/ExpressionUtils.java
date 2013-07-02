dfc74f271234d8dcee19b291697429f2dbd011e0
Emil Lundberg <emil.lundberg@sunstone.se>
2013-07-02 10:34:11 +0200

    Initial commit

---
+++ src/main/java/com/acme/el/ExpressionUtils.java
@@ -0,0 +1,9 @@
+package com.acme.el;
+
+public class ExpressionUtils {
+    public static Object evaluate() {
+        new org.apache.commons.el.ExpressionEvaluatorImpl();
+        return null;
+    }
+
+}
